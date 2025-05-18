82c62794304f87cfb09223c18aed130f985ad978
arjun98k <arjunkandekar14@gmail.com>
2025-05-18 16:54:48 +0530

    anonmous function and normal function

--- basic/basic_func.dart
+++ basic/basic_func.dart
@@ -3,6 +3,16 @@ void main(){
   var fun1 = (int a , int b){
     return a+b;
   };
+  int Function(int, int) fun2 = (int a, int b) {
+    return a + b;
+  };
 
   print( fun1(22,34));
+
+
+  int addition(int e , int d){
+    int c = e+d;
+    return c;
+  }
+  print(addition(12, 98));
 }
