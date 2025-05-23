cafefd58497fe26f4b9f130dbf3d41d442ca4de4
arjun98k <arjunkandekar14@gmail.com>
2025-05-23 23:47:46 +0530

    collection in dart

--- cfindart/collection1.dart
+++ cfindart/collection1.dart
@@ -2,7 +2,8 @@ void  main(){
 List<String> cake = ["chococlate","apple","mango"];
 cake.add("rasmalai");
 cake.reversed;
-for(String i in cake){
+print(cake.contains("apple"));
+  for(String i in cake){
   print(i);
 }
 }
