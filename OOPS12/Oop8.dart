591b3931360e13e3bdc02661bd66e49408979b10
arjun98k <arjunkandekar14@gmail.com>
2025-05-29 23:34:19 +0530

    abstract class

---
+++ OOPS12/Oop8.dart
@@ -0,0 +1,16 @@
+abstract class Arjun{
+  void whoIs(){
+
+  }
+}
+
+class Arjuninfo extends Arjun{
+  void whoIs(){
+    print('He is arjun kandekar and he is future engineer');
+  }
+}
+
+void main(){
+  Arjuninfo obj = new Arjuninfo();
+  obj.whoIs();
+}
